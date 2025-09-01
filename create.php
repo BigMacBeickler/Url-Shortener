99c9ae00f5304d1b9e67a01a56ce0ae540c6d77f
Chris <40841182+BigMacBeickler@users.noreply.github.com>
2025-09-01 17:38:01 +0200

    Update create.php
    KoMmEnTaRe

--- create.php
+++ create.php
@@ -1,14 +1,15 @@
+//Link creation. csfr check damit kein fremder Unfug anstellt. 
 <?php
 require_once __DIR__ . '/inc/functions.php';
 require_once __DIR__ . '/inc/header.php';
 require_login();
 $user = app_get_current_user();
-
+//Nur post, sicherheit.
 if($_SERVER['REQUEST_METHOD'] !== 'POST'){
     header('Location: index.php'); exit;
 }
 $token = $_POST['csrf'] ?? '';
-if(!csrf_check($token)) die('Ungültiges Formular.');
+if(!csrf_check($token)) die('Ungültiges Formular.'); //die --> nicht der Artikel, sondern stirb
 $long = trim($_POST['long_url'] ?? '');
 $slug = trim($_POST['slug'] ?? '');
 $description = trim($_POST['description'] ?? '');
@@ -19,8 +20,8 @@ if(!filter_var($long, FILTER_VALIDATE_URL)){
 if($slug === ''){
     $slug = generate_slug(6);
 } else {
-    // prüfen auf zulässige zeichen & existenz
-    if(!preg_match('/^[A-Za-z0-9_-]{3,}$/', $slug)) die('Ungültiger Slug.');
+    // prüfen auf zulässige zeichen und ob bereits vorhanden
+    if(!preg_match('/^[A-Za-z0-9_-]{3,}$/', $slug)) die('Ungültiger Slug.'); //reg-ex magic
     if(find_url_by_slug($slug)) die('Slug existiert bereits.');
 }
 if(save_new_url($user['id'], $slug, $long, $description)){
