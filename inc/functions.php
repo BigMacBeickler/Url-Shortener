d9b977d253f5a3f3e3d7ef586bfd778fa52911cd
Chris <40841182+BigMacBeickler@users.noreply.github.com>
2025-09-01 17:21:07 +0200

    Update functions.php
    Kommis

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,16 @@
 <?php
 // zentrale Hilfsfunktionen
+//?php nicht schließen weil https://stackoverflow.com/questions/4410704/why-would-one-omit-the-close-tag
 
+//wo ist was
 define('DATA_DIR', __DIR__ . '/../data');
 define('USERS_FILE', DATA_DIR . '/users.json');
 define('URLS_FILE', DATA_DIR . '/urls.json');
 
+//check for session, if none, start one
 if (session_status() === PHP_SESSION_NONE) session_start();
 
+//Daten vorhanden? wenn nicht erstellen
 function ensure_data_files(){
     if(!is_dir(DATA_DIR)) mkdir(DATA_DIR, 0777, true);
     if(!file_exists(USERS_FILE)) file_put_contents(USERS_FILE, json_encode([]), LOCK_EX);
@@ -14,17 +18,18 @@ function ensure_data_files(){
 }
 ensure_data_files();
 
+//daten laden
 function load_json($file){
     $fp = fopen($file, 'r');
     if(!$fp) return [];
-    flock($fp, LOCK_SH);
+    flock($fp, LOCK_SH);  //datei sperren, kein mehrfachzugriff. Doof bei großer userbase?
     $json = stream_get_contents($fp);
     flock($fp, LOCK_UN);
     fclose($fp);
     $data = json_decode($json, true);
     return is_array($data) ? $data : [];
 }
-
+//datei speichern, mit lock damit exklusiver zurgriff
 function save_json($file, $data){
     $fp = fopen($file, 'c+');
     if(!$fp) return false;
@@ -35,7 +40,7 @@ function save_json($file, $data){
     }
     ftruncate($fp, 0);
     rewind($fp);
-    $written = fwrite($fp, json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+    $written = fwrite($fp, json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)); //schöne formatierung und Schrägstriche werden nicht escaped.
     fflush($fp);
     flock($fp, LOCK_UN);
     fclose($fp);
@@ -43,16 +48,17 @@ function save_json($file, $data){
 }
 
 // Benutzerfunktionen
+//selbsterklärend
 function find_user_by_username($username){
     $users = load_json(USERS_FILE);
-    foreach($users as $u) if(strtolower($u['username']) === strtolower($username)) return $u;
+    foreach($users as $u) if(strtolower($u['username']) === strtolower($username)) return $u; //alles kleinschreiben und checken. Nicht case sensitiv, in anleitung! wird aber auch beim erstellen gecheckt
     return null;
 }
 
 function create_user($username, $password){
     $users = load_json(USERS_FILE);
     if(find_user_by_username($username)) return false;
-    $id = uniqid('u', true);
+    $id = uniqid('u', true); //more entropy!!
     $users[] = [
         'id' => $id,
         'username' => $username,
@@ -68,6 +74,7 @@ function verify_user($username, $password){
     return password_verify($password, $user['password']) ? $user : false;
 }
 
+//current_user gibts schon von php, also anderer name. Danke chatgpt für die Info ....
 function app_get_current_user(){
     if(!empty($_SESSION['user_id'])){
         $users = load_json(USERS_FILE);
@@ -76,6 +83,7 @@ function app_get_current_user(){
     return null;
 }
 
+//back to login.php if necessary
 function require_login(){
     if(!app_get_current_user()){
         header('Location: login.php');
@@ -84,6 +92,19 @@ function require_login(){
 }
 
 // URL-Funktionen
+/*slug --> ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡀⠀⣿⣿⠆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⠇⠀⣿⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣧⣴⣶⣿⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⠇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⠿⠋⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀
+⠀⠀⣀⣀⣤⣴⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀
+⠐⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀*/
+//slug = eindeutige identifiezierungsteil einer webadresse. https://developer.mozilla.org/en-US/docs/Glossary/Slug&hl=de&sl=en&tl=de&client=rq
+
 function generate_slug($len = 6){
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     $urls = load_json(URLS_FILE);
@@ -97,12 +118,14 @@ function generate_slug($len = 6){
     return $slug;
 }
 
+//für die weiterleitung auf gespeicherte url
 function find_url_by_slug($slug){
     $urls = load_json(URLS_FILE);
     foreach($urls as $u) if($u['slug'] === $slug) return $u;
     return null;
 }
 
+//neue url speichern
 function save_new_url($user_id, $slug, $long_url, $description = ''){
     $urls = load_json(URLS_FILE);
     $id = uniqid('r', true);
@@ -118,6 +141,7 @@ function save_new_url($user_id, $slug, $long_url, $description = ''){
     return save_json(URLS_FILE, $urls);
 }
 
+//für bearbeitungsmodus
 function update_url($id, $user_id, $new_long_url, $description = ''){
     $urls = load_json(URLS_FILE);
     $changed = false;
@@ -132,6 +156,8 @@ function update_url($id, $user_id, $new_long_url, $description = ''){
     return false;
 }
 
+
+//joa, löschen halt, ney
 function delete_url($id, $user_id){
     $urls = load_json(URLS_FILE);
     $new = [];
@@ -144,6 +170,7 @@ function delete_url($id, $user_id){
     return false;
 }
 
+//counter für Besuche
 function increment_visit($slug){
     $urls = load_json(URLS_FILE);
     $changed = false;
@@ -154,6 +181,7 @@ function increment_visit($slug){
 }
 
 // CSRF token (einfach)
+//safety first! Check for correct Token. https://www.gnucitizen.org/blog/csrf-demystified/
 function csrf_token(){
     if(empty($_SESSION['csrf_token'])){
         $_SESSION['csrf_token'] = bin2hex(random_bytes(16));
